0957240564c2ca4551a1090fbee055a6d06243b8
Sam Gardner <samgardner20@gmail.com>
2021-07-18 18:11:04 -0400

    This is a fully operational RPS JS console game. bare bones but it works

--- app.js
+++ app.js
@@ -1,68 +1,67 @@
-// This is a function called computerPlay that will randomly return either rock paper or scissors
+let userScore = 0;
+let compScore = 0;
+console.log("This is a JS console game of RPS. You will play a total of five rounds. Ties will award no points. The final scores will be tallied up at the end");
 
-function computerPlay () {
+
+function singleGame(userChoice, compChoice) {
+//user prompt and input
+    userChoice = prompt("Rock, Paper, or Scissors?", "");
+    userSelection = userChoice.toLowerCase();
+
+// randomly generated computer answer
     compChoice = Math.floor(Math.random ()* 3);
-    if (compChoice === 0) {
-        computerSelection ="rock";
-    }
-    else if (compChoice === 1) {
-        computerSelection = "paper";
-    }
-    else {
-        computerSelection = "scissors";
-    }
-}
-computerPlay();
-
-
-
-function playerPlay () {
-    playerChoice = prompt ("Rock, Paper, or Scissors?", "");
-    playerSelection = playerChoice.toLowerCase();
-    return playerSelection;
-
-}
-playerPlay();
-
-function singleRound(playerSelection, computerSelection){
-   switch (playerSelection) {
-        case "rock":
-            if (computerSelection === "paper"){
-                console.log("Paper beats rock. You Lose!");
+        if (compChoice === 0) {
+                computerSelection ="rock";
             }
-            else {
-                console.log("Rock beats scissors. You Win!");
+            else if (compChoice === 1) {
+                computerSelection = "paper";
             }
-            break;
-        case "paper":
-            if (computerSelection === "scissors"){
-                return "Scissors beats paper. You Lose!";
-                }
-            else {
-                return "Paper beats rock. You Win!";
-                }
-            break;
-        case "scissors":
-            if (computerSelection === "rock"){
-                return "Rock beats scissors. You Lose!";
-                }
-            else {
-                return "Scissors beats paper. You Win!";
-                }
-            break;
-        }
+            else if (compChoice === 2) {
+                computerSelection = "scissors";
+            }
+
+//compare the two values
+if (userSelection === computerSelection) {
+    console.log("tie");
+    
+}
+else if (userSelection !== computerSelection) {
+    winOrLose(userSelection, computerSelection);
+}
+// function if it is not a tie
+function winOrLose (userSelection, computerSelection) {
+    if ((userSelection === "rock" && computerSelection === "scissors") || (userSelection === "paper" && computerSelection === "rock") || (userSelection === "scissors" && computerSelection === "paper")) {
+        userScore += 1;
+        console.log("you have won");
+        console.log(`your score is ${userScore}`);
+        console.log(`the computer score is ${compScore}`);
+    }
+    else if ((userSelection === "rock" && computerSelection === "paper") || (userSelection === "paper" && computerSelection === "scissors") || (userSelection === "scissors" && computerSelection === "rock")) {
+       compScore += 1;
+       console.log("you lost"); 
+       console.log(`your score is ${userScore}`);
+       console.log(`the computer score is ${compScore}`);
+}
+}
+}
+
+//repeat the game five times
+for (i=0; i<5; i++){
+    singleGame();
+    
+}
+console.log(`Your Final Score is ${userScore}`);
+console.log(`The Computers Final Score is ${compScore}`);
+
+if (userScore > compScore) {
+    console.log("YOU BEAT THE COMPUTER");
+}
+else if ( userScore >compScore) {
+    console.log("THE COMPUTER WIPED THE FLOOR WITH YOU. BETTER LUCK NEXT TIME");
+}
+else {
+    console.log("YOU SOMEHOW TIED ALL FIVE ROUNDS. TRY AGAIN!")
 }
 
 
 
-
-function game () {
-    singleRound(playerSelection, computerSelection);
-
-
-    
-}
-
-
-    
-    
